ae66c9ccebdc202faac9c5ac0c5060ef77207ae6
Everth Paredes <epe0413@gmail.com>
2026-05-12 21:32:02 -0600

    Sección 4: Creación de categoria - Finalizada

---
+++ Data/ApplicationDbContext.cs
@@ -0,0 +1,14 @@
+﻿using ApiEcommerce_VS.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace ApiEcommerce_VS.Data
+{
+    public class ApplicationDbContext:DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+            
+        }
+        public DbSet<Category> Categories { get; set; }
+    }
+}
---
+++ Models/Category.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ApiEcommerce_VS.Models
+{
+    public class Category
+    {
+        [Key]
+        public int Id { get; set; }
+        [Required]
+        public string name { get; set; } = string.Empty;
+        [Required]
+        public DateTime CreationDate {  get; set; }
+    }
+}
