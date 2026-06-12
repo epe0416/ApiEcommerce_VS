d86686fc1922d7387721b2b105999c10ed4956e6
Everth Paredes <epe0413@gmail.com>
2026-06-12 12:21:37 -0600

    Creacion del modelo de User

---
+++ Models/User.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ApiEcommerce_VS.Models
+{
+    public class User
+    {
+        [Key]
+        public int Id { get; set; }
+        public string? Name { get; set; }
+        public string? UserName { get; set; }
+        public string? Password { get; set; }
+        public string? Rol { get; set; }
+    }
+}
