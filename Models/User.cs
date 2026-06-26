cb6075569ad41977595fb02fb9f5c4eff96b4bab
Everth Paredes <epe0413@gmail.com>
2026-06-26 11:48:13 -0600

    Implementar interfaz usuario

--- Models/User.cs
+++ Models/User.cs
@@ -7,7 +7,7 @@ namespace ApiEcommerce_VS.Models
         [Key]
         public int Id { get; set; }
         public string? Name { get; set; }
-        public string? UserName { get; set; }
+        public string UserName { get; set; } = string.Empty;
         public string? Password { get; set; }
         public string? Rol { get; set; }
     }
---
+++ Repository/UserRepository.cs
@@ -0,0 +1,40 @@
+﻿using ApiEcommerce_VS.Data;
+using ApiEcommerce_VS.Models;
+using ApiEcommerce_VS.Models.Dtos;
+using ApiEcommerce_VS.Repository.IRepository;
+
+namespace ApiEcommerce_VS.Repository
+{
+    public class UserRepository : IUserRepository
+    {
+        public readonly ApplicationDbContext _db;
+        public UserRepository(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+        public User? GetUser(int id)
+        {
+            return _db.Users.FirstOrDefault(u => u.Id == id);
+        }
+
+        public ICollection<User> GetUsers()
+        {
+            return _db.Users.OrderBy(u => u.UserName).ToList();
+        }
+
+        public bool IsUniqueUser(string username)
+        {
+            return !_db.Users.Any(u => u.UserName.ToLower().Trim() == username.ToLower().Trim());
+        }
+
+        public Task<UserLoginDto> Login(UserLoginDto userLoginDto)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<User> Register(CreateUserDto createUserDto)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
