ae03626d10c8c3cc35928bfda24233b2c07be69c
Everth Paredes <epe0413@gmail.com>
2026-07-13 05:42:08 -0600

    Acceso a usuario (login)

--- Controllers/UsersController.cs
+++ Controllers/UsersController.cs
@@ -69,5 +69,25 @@ namespace ApiEcommerce_VS.Controllers
             }
             return CreatedAtRoute("GetUser", new { id = result.Id }, result);
         }
+
+        [HttpPost("Login", Name = "LoginUser")]
+        [ProducesResponseType(StatusCodes.Status403Forbidden)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status500InternalServerError)]
+        public async Task<IActionResult> LoginUser([FromBody] UserLoginDto userLoginDto)
+        {
+            if (userLoginDto == null || !ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+            
+            var user = await _userRepository.Login(userLoginDto);
+            if (user == null)
+            {
+                return Unauthorized();
+            }
+            return Ok(user);
+        }
     }
 }
