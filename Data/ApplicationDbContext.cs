96945af0768e43ea120d5761561cfeee1ad926eb
Everth Paredes <epe0413@gmail.com>
2026-09-04 05:22:03 -0600

    Migración para uso de Identity

--- Data/ApplicationDbContext.cs
+++ Data/ApplicationDbContext.cs
@@ -1,9 +1,10 @@
 ﻿using ApiEcommerce_VS.Models;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 
 namespace ApiEcommerce_VS.Data
 {
-    public class ApplicationDbContext:DbContext
+    public class ApplicationDbContext:IdentityDbContext<ApplicationUser>
     {
         public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
         {
