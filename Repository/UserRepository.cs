33f62b3c1f09edebcc76b686fe88a34005a759d7
Everth Paredes <epe0413@gmail.com>
2026-09-05 00:21:11 -0600

    Pruebas de funcionalidad y correcciones

--- Repository/UserRepository.cs
+++ Repository/UserRepository.cs
@@ -145,7 +145,8 @@ namespace ApiEcommerce_VS.Repository
                 var createdUser = _db.ApplicationUsers.FirstOrDefault(u => u.UserName == createUserDto.Username);
                 return _mapper.Map<UserDataDto>(createdUser);
             }
-            throw new ApplicationException("No se pudo realizar el registro");
+            var erros = string.Join(", ", result.Errors.Select(e => e.Description));
+            throw new ApplicationException($"No se pudo realizar el registro: {erros}");
         }
     }
 }
